f6b19af178e4f63831fc26921627957c4702d7f4
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-22 19:23:06 +0530

    " setup is completed of redux-saga"

--- app/StoreProvider.jsx
+++ app/StoreProvider.jsx
@@ -3,7 +3,9 @@ import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from '../store/store.js'
 
+
 export default function StoreProvider({ children }) {
+
   const storeRef = useRef()
   if (!storeRef.current) {
     // Create the store instance the first time this renders
--- store/store.js
+++ store/store.js
@@ -1,10 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '@/store/auth/auth.js'
+import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware } from '@reduxjs/toolkit'
+// import reduxSaga from 'redux-saga'
+import rootSaga from './sagas'
 
 export const makeStore = () => {
-  return configureStore({
+
+  const sagaMiddleware = createSagaMiddleware()
+
+  const store = configureStore({
     reducer: {
         counter: counterReducer, 
     },
+   middleware : ()=> [sagaMiddleware]
+
   })
+
+  sagaMiddleware.run(rootSaga)
+ 
+  return store
 }
